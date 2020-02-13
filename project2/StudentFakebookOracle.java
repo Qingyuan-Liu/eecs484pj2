a00ba21a323f974b73e35a7600e49509fa9c2590
Qingyuan Liu <qyyf@umich.edu>
2020-02-13 16:05:33 -0500

    Update StudentFakebookOracle.java

--- project2/StudentFakebookOracle.java
+++ project2/StudentFakebookOracle.java
@@ -218,7 +218,7 @@ public final class StudentFakebookOracle extends FakebookOracle {
              * UserInfo(104, "Tom", "Hanks"); results.add(u1); results.add(u2);
              */
             ResultSet rst = stmt.executeQuery("SELECT USER_ID, FIRST_NAME, LAST_NAME" + " FROM " + UsersTable  + " MINUS " + " SELECT USER_ID " +
-            "FROM "+ CurrentCitiesTable + " P1," +  HometownCitiesTable +" P2" + " WHERE P1.USER_ID = P2.USER_ID AND P1.CURRENT_CITY_ID=P2.CURRENT_CITY_ID ORDER BY 1");
+            "FROM "+ CurrentCitiesTable + " P1," +  HometownCitiesTable +" P2" + " WHERE P1.USER_ID = P2.USER_ID AND P1.CURRENT_CITY_ID=P2.HOMETOWN_CITY_ID ORDER BY 1");
             
             while(rst.next()){
                 results.add(new UserInfo(rst.getLong(1),rst.getString(2),rst.getString(3)));
