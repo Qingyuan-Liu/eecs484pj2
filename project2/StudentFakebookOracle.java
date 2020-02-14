8b95c7e4e2b89931a07d7f22f5056383943ba6ea
Qingyuan Liu <qyyf@umich.edu>
2020-02-13 23:21:16 -0500

    Update StudentFakebookOracle.java

--- project2/StudentFakebookOracle.java
+++ project2/StudentFakebookOracle.java
@@ -392,7 +392,7 @@ public final class StudentFakebookOracle extends FakebookOracle {
              */
 
             ResultSet rst = stmt.executeQuery("SELECT MAX(COUNT(C.STATE_NAME)) "+"FROM "+
-            CurrentCitiesTable + " C, " + EventsTable + " E "+
+            CitiesTable + " C, " + EventsTable + " E "+
             "WHERE C.CITY_ID = E.EVENT_CITY_ID "+" GROUP BY C.STATE_NAME");
             
             int maxCount=0;
